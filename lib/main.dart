c8ef0670b1e2c70c4b0b4397ccc44f87fe7a6d47
Divine Bayuo <divinewb123@gmail.com>
2026-03-04 22:15:46 +0000

    Add firebase config file to gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,6 @@ void main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  /* await Firebase.initializeApp(
-      options: FirebaseOptions(
-    apiKey: dotenv.env['FIREBASE_API_KEY']!,
-    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
-    projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
-    storageBucket: dotenv.env['FIREBASE_STORAGEBUCKET']!,
-    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
-    appId: dotenv.env['FIREBASE_API_APPID']!,
-    measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID']!,
-  )); */
-
   runApp(const MyApp());
 }
 
